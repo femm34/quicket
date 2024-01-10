dddf13b0068b1e84a6ee03648c8980a79739d612
Ivan0301004 <ignizbones@gmail.com>
2024-01-10 12:13:14 -0600

    Entities created

--- src/sign-in-details/entities/sign-in-detail.entity.ts
+++ src/sign-in-details/entities/sign-in-detail.entity.ts
@@ -2,9 +2,11 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'sign_in_details' })
 export class SignInDetail {
+
   @PrimaryGeneratedColumn()
   private sessionId: number;
 
   @Column()
   private loggedIsOn: boolean;
+  
 }
